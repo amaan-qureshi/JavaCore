f33bbb403814bf16f6e524134dbcd266cbb591d3
amaan.qureshi <amaan.qureshi@emaratech.ae>
2018-11-20 08:48:41 +0400

    initial commit

---
+++ JavaExercise/src/main/java/com/core/structure/CustomMap.java
@@ -0,0 +1,4 @@
+package com.core.structure;
+
+public class CustomMap {
+}
---
+++ JavaExercise/src/test/java/com/core/structure/CustomMapTest.java
@@ -0,0 +1,13 @@
+package com.core.structure;
+
+
+import org.junit.Test;
+
+public class CustomMapTest {
+
+    @Test
+    public void testMyMap(){
+
+    }
+
+}
